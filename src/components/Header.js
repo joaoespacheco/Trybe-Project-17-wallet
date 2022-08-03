cf05168564b24d33c5291e20f517a89bfc1b4802
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-08-03 19:20:38 -0300

    Conclusão do requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <div>
+          <p data-testid="email-field">{email}</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+      </header>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <main>
+        <Header
+          { ...this.props }
+        />
+        <WalletForm />
+        <Table />
+      </main>
+    );
   }
 }
 
