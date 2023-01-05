f9e30c153b24a2f2f0ec7ae3e8ab49fb8f015a55
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-01-05 17:40:08 -0300

    Adição da pasta styles e alteração do pacote sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './styles/main.scss';
 import App from './App';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
